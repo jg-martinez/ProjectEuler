a2eaf1c44597260b3e80efaa75f6ca4431388348
Juan Gabriel <jg.martinez90@gmail.com>
2014-07-28 16:38:16 +0200

    Solution to problem 004

--- 004.py
+++ 004.py
@@ -7,5 +7,16 @@ from the product of two 2-digit numbers is 9009 = 91 * 99.
 
 Find the largest palindrome made from the product of two 3-digit numbers.
 """
+def isPalindrome(number):
+	return str(number) == str(number)[::-1]
+	
+def solve():
+	result = 0
+	for i in range(999,100,-1):
+		for j in range(999, 100, -1):
+			if (isPalindrome(i*j) and (i*j) > result):
+				result = i * j
+	return result
 
-
+if __name__=="__main__":
+	print (solve())
